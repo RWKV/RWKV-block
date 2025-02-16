68c3db4c6c7d0555654712e74e603ea2f31cba61
Eugene Cheah (picocreator) <picocreator@gmail.com>
2025-02-16 03:19:15 +0000

    fix mismatched use_v_first

--- rwkv_block/v7_qwerky/model/qwerky7_model.py
+++ rwkv_block/v7_qwerky/model/qwerky7_model.py
@@ -224,7 +224,7 @@ class Qwerky7Model(nn.Module):
         v_first = None
 
         # Uses the input hidden state, as the v_first if v_first_embedding is enabled
-        if self.configMap.use_v_first:
+        if self.configMap.v_first_embedding:
             v_first = x_hidden_state.clone()
 
         # Force overwrite_ret_tensor to False, if ret_stateList is None
